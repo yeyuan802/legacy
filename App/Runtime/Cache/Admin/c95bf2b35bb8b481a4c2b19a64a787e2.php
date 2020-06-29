dbde2aa0d6fa6ebf3e9664886519e54fb098b34c
baojing <1461729781@qq.com>
2020-06-29 18:48:16 +0800

    解决冲突

--- App/Runtime/Cache/Admin/c95bf2b35bb8b481a4c2b19a64a787e2.php
+++ App/Runtime/Cache/Admin/c95bf2b35bb8b481a4c2b19a64a787e2.php
@@ -39,29 +39,17 @@
                     <li class="dropdown">
                         <a href="/legacy/Admin/Goods/index" class="dropdown-toggle active2" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 商品管理<b class="caret"></b></a>
                         <ul class="dropdown-menu">
-<<<<<<< HEAD
                             <li><a href="/legacy/Admin/Goods/index">查看商品</a></li>
                             <li><a href="/legacy/Admin/Goods/add">添加商品</a></li>
                             <li><a href="/legacy/Admin/Goods/search">查询商品</a></li>
-=======
-                            <li><a href="/EPP_Project/legacy/Admin/Goods/index">查看商品</a></li>
-                            <li><a href="/EPP_Project/legacy/Admin/Goods/add">添加商品</a></li>
-                            <li><a href="/EPP_Project/legacy/Admin/Goods/search">查询商品</a></li>
->>>>>>> orgin/master
                         </ul>
                     </li>
                     <li class="dropdown">
                         <a href="/legacy/Admin/Travel/index" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span> 旅游管理<b class="caret"></b></a>
                         <ul class="dropdown-menu">
-<<<<<<< HEAD
                             <li><a href="/legacy/Admin/Travel/index">查看景点</a></li>
                             <li><a href="/legacy/Admin/Travel/add">增加景点</a></li>
                             <li> <a href="/legacy/Admin/Travel/search">查询景点</a></li>
-=======
-                            <li><a href="/EPP_Project/legacy/Admin/Travel/index">查看景点</a></li>
-                            <li><a href="/EPP_Project/legacy/Admin/Travel/add">增加景点</a></li>
-                            <li> <a href="/EPP_Project/legacy/Admin/Travel/search">查询景点</a></li>
->>>>>>> orgin/master
                         </ul>
                     </li>
                     <li class="dropdown">
@@ -84,27 +72,16 @@
                             <li class="dropdown-submenu">
                                 <a href="/legacy/Admin/Inheritor/index" class="dropdown-toggle" data-toggle="dropdown">传承人管理</a>
                                 <ul class="dropdown-menu">
-<<<<<<< HEAD
                                     <li><a href="/legacy/Admin/Inheritor/index">查看传承人</a></li>
                                     <li><a href="/legacy/Admin/Inheritor/add">增加传承人</a></li>
                                     <li><a href="/legacy/Admin/Inheritor/search">查询传承人</a></li>
-=======
-                                    <li><a href="/EPP_Project/legacy/Admin/Inheritor/index">查看传承人</a></li>
-                                    <li><a href="/EPP_Project/legacy/Admin/Inheritor/add">增加传承人</a></li>
-                                    <li><a href="/EPP_Project/legacy/Admin/Inheritor/search">查询传承人</a></li>
->>>>>>> orgin/master
                                 </ul>
                             </li>
                             <li class="dropdown-submenu">
                                 <a href="/legacy/Admin/Inheritor/index" class="dropdown-toggle" data-toggle="dropdown">政策管理</a>
                                 <ul class="dropdown-menu">
-<<<<<<< HEAD
                                     <li><a href="/legacy/Admin/Policy/index">查看政策</a></li>
                                     <li><a href="/legacy/Admin/Policy/add">增加政策</a></li>
-=======
-                                    <li><a href="/EPP_Project/legacy/Admin/Policy/index">查看政策</a></li>
-                                    <li><a href="/EPP_Project/legacy/Admin/Policy/add">增加政策</a></li>
->>>>>>> orgin/master
                                 </ul>
                             </li>
                         </ul>
@@ -123,9 +100,6 @@
                     <a href="#"  class="dropdown-toggle " data-toggle="dropdown">admin<b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li><a href="/legacy/"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>前台首页</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>个人设置</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>账户中心</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>我的收藏</a></li>
                         </ul>
                     </li>
                     <li><a href="/legacy/Admin/Login/logout"><span class="glyphicon glyphicon-off" aria-hidden="true"></span>退出</a></li>
@@ -157,13 +131,8 @@
 <body>
 <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
             <ul class="list-group">
-<<<<<<< HEAD
-                <a href="/legacy/Admin/Guest/index" class="list-group-item active">查看顾客信息</a>
+                <a href="/legacy/Admin/Guest/index" class="list-group-item active">查看顾客</a>
                 <a href="/legacy/Admin/Guest/searchguest" class="list-group-item">查询顾客</a>
-=======
-                <a href="/EPP_Project/legacy/Admin/Guest/index" class="list-group-item active">查看顾客</a>
-                <a href="/EPP_Project/legacy/Admin/Guest/searchguest" class="list-group-item">查询顾客</a>
->>>>>>> orgin/master
             </ul>
         </div>
         <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10">
@@ -197,7 +166,7 @@
                                                 <th scope="row"></th>
                                                 <td><?php echo ($v["uid"]); ?></td>
                                                 <td><?php echo ($v["nickname"]); ?></td>
-                                                <td><a href="/EPP_Project/legacy/Admin/Guest/order/uid/<?php echo ($v["uid"]); ?>">查看详情</a></td>
+                                                <td><a href="/legacy/Admin/Guest/order/uid/<?php echo ($v["uid"]); ?>">查看详情</a></td>
                                                 <td><?php echo ($v["email"]); ?></td>
                                                 <td><?php echo ($v["phone"]); ?></td>
                                                 <td><?php echo ($v["address"]); ?></td>
